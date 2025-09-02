aa0bdc4d835256f7ec5a98cf30768bbe71592b82
schmalzl <kian.schmalzl@icloud.com>
2025-09-02 23:58:02 +0200

    add: basic window gui (not including callbacks)

---
+++ src/gui.py
@@ -0,0 +1,50 @@
+import dearpygui.dearpygui as dpg # type: ignore
+from src.add import add_to_actionlist
+import src.console as console
+from src.menu_callbacks import callback_install_build_deps
+
+def display_gui():
+    with dpg.viewport_menu_bar():
+        with dpg.menu(label="File"):
+            dpg.add_menu_item(label="New Project")
+            dpg.add_menu_item(label="Open Project File")
+            dpg.add_separator()
+            dpg.add_menu_item(label="Import new Feature")
+            dpg.add_separator()
+            dpg.add_menu_item(label="Save")
+            dpg.add_menu_item(label="Save As...")
+            dpg.add_separator()
+            dpg.add_menu_item(label="Preferences")
+            dpg.add_menu_item(label="Exit")
+        with dpg.menu(label="Project"):
+            dpg.add_menu_item(label="Project Settings")
+        with dpg.menu(label="Build"):
+            dpg.add_menu_item(label="Update build dependencies", callback=callback_install_build_deps)
+            dpg.add_separator()
+            dpg.add_menu_item(label="Build Debug")
+            dpg.add_menu_item(label="Build Release")
+        with dpg.menu(label="Help"):
+            dpg.add_menu_item(label="Support & Help")
+            dpg.add_separator()
+            dpg.add_menu_item(label="See current version")
+            dpg.add_menu_item(label="View License")
+            dpg.add_menu_item(label="About")
+        dpg.add_separator()
+        dpg.add_text("Current Project: none")
+
+    with dpg.window(label="Actions", width=210, height=782, no_close=True, no_resize=True, no_move=True, no_collapse=True):
+        dpg.add_text("-- OUTPUT --")
+        dpg.add_text("Print Text to console")
+        dpg.add_same_line()
+        dpg.add_button(label="Add", callback=add_to_actionlist, user_data="_print")
+        dpg.add_text("-- INPUT --")
+    
+    with dpg.window(label="Console", width=790, height=200, no_close=True, no_resize=True, no_move=True, no_collapse=True, pos=(210, 600)):
+        for elements in console.CONSOLE_BUFFER:
+            dpg.add_text(elements, color=(255, 255, 255, 200))
+    
+    with dpg.window(label="Project", width=790, height=582, no_close=True, no_resize=True, no_move=True, no_collapse=True, pos=(210, 0)):
+        pass
+
+    dpg.show_style_editor()
+    
