deb3366eb58b46bb3cd7ef2707c07c25fc9058eb
schmalzl <kian.schmalzl@icloud.com>
2025-09-03 02:33:32 +0200

    cleanup: comment main file better and restructure code

--- main.py
+++ main.py
@@ -1,35 +1,49 @@
-# clink v0.1.dev
+# clink v0.1.dev WIP - https://github.com/schmalzl/clink
+# Copyright (c) 2025 Kian Schmalzl
+# (program main entry point)
 
+# sys import for pycache deactivation
 import sys
-sys.dont_write_bytecode = True  # disable __pycache__ globally
-import platform # environment checks
+# disable __pycache__ creation globally
+sys.dont_write_bytecode = True
+
+# --- Main includes
+# import enviroment check with platform.system()
+import platform
+# import dear pygui (ui library)
 import dearpygui.dearpygui as dpg # type: ignore
 import dearpygui # type: ignore
-from src.gui import display_gui
+# import functions to load gui and update internal console
+from src.gui import load_gui
 from src.gui import add_to_console
 import src.gui as gui
+# shared package for specific container theme (console)
 import src.gui_ids as id
 
 def run():
-    # check the current system environment
+    # check current system environment (Windows, Mac, Linux)
     current_platform = platform.system()
     render_api = "misc"
     if current_platform == "Windows":
+        # DirectX 11 backend
         render_api = "DX11"
     elif current_platform == "Darwin" or current_platform == "Linux":
+        # OpenGL backend
         render_api = "GL"
 
-    dpg.create_context()
 
+    dpg.create_context()    # create dear pygui context
+    load_gui()              # load gui from gui.py
 
-    # load gui
-    display_gui()
-
+    # add startup message to console buffer
     gui.CONSOLE_BUFFER.append("clink studio v0.1.dev - https://github.com/schmalzl/clink")
     gui.CONSOLE_BUFFER.append("(c) 2025 Kian Schmalzl")
-    
 
-    # define window styles
+
+    # define global window style and color scheme
+    # Note: Other style variables can be added or changed for further customization
+    #       Please refer to https://dearpygui.readthedocs.io/en/latest/documentation/themes.html
+    #       for further information.
     with dpg.theme() as global_theme:
         with dpg.theme_component(dpg.mvAll):
             dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 0)
@@ -41,36 +55,35 @@ def run():
             dpg.add_theme_color(dpg.mvThemeCol_TitleBg, (40, 40, 40))
             dpg.add_theme_color(dpg.mvThemeCol_TitleBgActive, (40, 40, 40))
 
+    # define console-specific window style
     with dpg.theme() as console_theme:
         with dpg.theme_component(dpg.mvAll):
             dpg.add_theme_style(dpg.mvStyleVar_ItemSpacing, 5, 0)
             dpg.add_theme_style(dpg.mvStyleVar_FramePadding, 2, 0)
-    
-
 
     # apply styles
-    dpg.bind_theme(global_theme)
-    dpg.bind_item_theme(id._console_win, console_theme)
+    dpg.bind_theme(global_theme)                        # global
+    dpg.bind_item_theme(id._console_win, console_theme) # console specific
 
-    # set title
+
+    # set title and create viewport
     app_title = "clink studio 0.1.dev <" + render_api + ">"
-
     dpg.create_viewport(title=app_title, width=1000, height=800, resizable=False)
+    
     # set window icon
     dpg.set_viewport_large_icon("./assets/icons/icon_128.ico")
 
-    dpg.setup_dearpygui()
-    dpg.show_viewport()
+    dpg.setup_dearpygui()   # setup viewport
+    dpg.show_viewport()     # show viewport
 
     while dpg.is_dearpygui_running():
         dpg.render_dearpygui_frame()
-        
-        # --- custom code ---
+        # console update every frame until loop closed
         add_to_console()
-        
-
-
-    dpg.destroy_context()
+    
+    # --- program will stop here until the window is closed ---
+    # POST COMMAND EXECUTIONS
+    dpg.destroy_context()   # destroy dear pygui context
 
 if __name__ == "__main__":
     run()
